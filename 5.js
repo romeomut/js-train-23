4b24150d927f455c9dbcccd0a2bbf64b07e2675a
mut_mut <anhialo.mut@gmail.com>
2023-11-15 21:17:36 +0200

    js-train-23

--- 5.js
+++ 5.js
@@ -1,44 +1,65 @@
 // Адаптер (Adapter) — це патерн програмування, який дозволяє об'єктам з інтерфейсом несумісним з іншими об'єктами працювати разом,
 // перетворюючи інтерфейс одного об'єкта на інтерфейс, очікуваний іншим об'єктом.
-
+console.clear()
 // Клас BankTransfer представляє собою систему для здійснення банківських переказів
 class BankTransfer {
-  // Зробіть метод initiateTransfer, який приймає amount та відповідає за ініціювання банківського переказу
-  // Він приймає суму переказу як параметр
-  // Для ініціювання банківського переказу спершу обчислюється сума з урахуванням комісії calculatedAmount = this.calculateFee(amount)
-  // Виводимо інформацію про ініціювання банківського переказу Ініціюємо банківський переказ: $${calculatedAmount}
-  // Зробіть метод calculateFee, який відповідає за розрахунок комісії за переказ
-  // Він приймає amount переказу як параметр та повертає число після розрахування комісії
-  // Логіка розрахунку комісії за переказ amount * 1.02
-  // Припустимо, комісія становить 2% від суми переказу
-}
-
-// Клас WalletTransfer представляє собою систему для здійснення переказів з гаманця
-class WalletTransfer {
-  // Створіть метод processTransfer, який відповідає за здійснення переказу з гаманця
-  // Він приймає суму переказу як параметр
-  // Виводимо інформацію про здійснення переказу з гаманця Здійснюємо переказ з гаманця: $${amount}
-}
-
-// Клас TransferAdapter виступає адаптером, який дозволяє нам користуватися
-// методами WalletTransfer так, ніби це BankTransfer.
-class TransferAdapter {
-  // Робимо конструктор, що приймає об'єкт transferSystem типу WalletTransfer
-  // Зберігаємо посилання на об'єкт WalletTransfer у властивості transferSystem
-  // Робимо метод initiateTransfer, який адаптує API WalletTransfer до API BankTransfer.
-  // Він приймає amount як аргумент та повертає результат виконання переказу.
-  // Викликаємо допоміжний метод calculateFee для обчислення комісії за переказ та результат записуєм в константу calculatedAmount
-  // Викликаємо метод processTransfer об'єкту WalletTransfer з calculatedAmount.
-  // В результаті повертаємо результат виконання переказу.
-  // Створюємо метод calculateFee, що приймає amount та обчислює суму комісії за переказ amount * 1.2, засновуючись на вхідній сумі.
-  // Повертаємо amount * 1.2
-}
-console.log("Завдання 5 ====================================");
-// Після виконання розкоментуйте код нижче
-
-// Створимо екземпляри BankTransfer
-// const purchase1 = new BankTransfer();
-// purchase1.initiateTransfer(1000);
-
-// const purchase2 = new BankTransfer();
-// purchase2.initiateTransfer(10);
+    // Зробіть метод initiateTransfer, який приймає amount та відповідає за ініціювання банківського переказу
+    // Він приймає суму переказу як параметр
+    // Для ініціювання банківського переказу спершу обчислюється сума з урахуванням комісії calculatedAmount = this.calculateFee(amount)
+    // Виводимо інформацію про ініціювання банківського переказу Ініціюємо банківський переказ: $${calculatedAmount}
+    initiateTransfer(amount) {
+        const calculatedAmount = this.calculateFee(amount);
+        console.log(`Ініціюємо банківський переказ: $${calculatedAmount}`);
+    }
+    // Зробіть метод calculateFee, який відповідає за розрахунок комісії за переказ
+    // Він приймає amount переказу як параметр та повертає число після розрахування комісії
+    // Логіка розрахунку комісії за переказ amount * 1.02
+    // Припустимо, комісія становить 2% від суми переказу
+    calculateFee(amount) {
+        return amount * 1.02;
+    }
+  }
+  
+  // Клас WalletTransfer представляє собою систему для здійснення переказів з гаманця
+  class WalletTransfer {
+    // Створіть метод processTransfer, який відповідає за здійснення переказу з гаманця
+    // Він приймає суму переказу як параметр
+    // Виводимо інформацію про здійснення переказу з гаманця Здійснюємо переказ з гаманця: $${amount}
+    processTransfer(amount) {
+        console.log(`Здійснюємо переказ з гаманця: $${amount}`);
+    }
+  }
+  
+  // Клас TransferAdapter виступає адаптером, який дозволяє нам користуватися
+  // методами WalletTransfer так, ніби це BankTransfer.
+  class TransferAdapter {
+    // Робимо конструктор, що приймає об'єкт transferSystem типу WalletTransfer
+    constructor(transferSystem) {
+        this.transferSystem = transferSystem;
+    }
+    // Зберігаємо посилання на об'єкт WalletTransfer у властивості transferSystem
+    // Робимо метод initiateTransfer, який адаптує API WalletTransfer до API BankTransfer.
+    // Він приймає amount як аргумент та повертає результат виконання переказу.
+    // Викликаємо допоміжний метод calculateFee для обчислення комісії за переказ та результат записуєм в константу calculatedAmount
+    // Викликаємо метод processTransfer об'єкту WalletTransfer з calculatedAmount.
+    // В результаті повертаємо результат виконання переказу.
+    // Створюємо метод calculateFee, що приймає amount та обчислює суму комісії за переказ amount * 1.2, засновуючись на вхідній сумі.
+    // Повертаємо amount * 1.2
+    initiateTransfer(amount) {
+        const calculatedAmount = this.calculateFee(amount);
+        return this.transferSystem.processTransfer(calculatedAmount);
+      }
+      calculateFee(amount) {
+        return amount * 1.2;
+    }
+  }
+  console.log("Завдання 5 ====================================");
+  // Після виконання розкоментуйте код нижче
+  
+  // Створимо екземпляри BankTransfer
+  const purchase1 = new BankTransfer();
+  purchase1.initiateTransfer(1000);
+  
+  const purchase2 = new BankTransfer();
+  purchase2.initiateTransfer(10);
+  
--- 6.js
+++ 6.js
@@ -1,44 +1,66 @@
 // Стратегія (Strategy) — це патерн програмування, який дозволяє визначати різні алгоритми та забезпечує можливість обміну їх під час виконання програми.
-
+console.clear()
 // Клас Basket представляє кошик для покупок з певною стратегією знижки
 class Basket {
-  // Створимо конструктор приймає, що стратегію знижки discountPlan як параметр
-  // Властивість discountPlan отримує значення стратегії знижки, яке було передано конструктору
-  // Створюємо новий пустий масив для зберігання товарів (goods) в кошику
-  // Робимо метод addGood, що приймає один параметр - good, який потрібно додати до масиву
-  // Додаємо новий товар в масив товарів
-  // Робимо метод calculateTotalPrice, що розраховує загальну вартість товарів в кошику з урахуванням знижки
-  // За допомогою метода reduce ми сумуємо вартість всіх товарів в масиві
-  // Застосовуємо знижку до загальної вартості за допомогою метода applyDiscount нашого об'єкта discountPlan
-}
+    // Створимо конструктор приймає, що стратегію знижки discountPlan як параметр
+    // Властивість discountPlan отримує значення стратегії знижки, яке було передано конструктору
+    // Створюємо новий пустий масив для зберігання товарів (goods) в кошику
+    goods = [];
 
-// Клас RegularDiscountPlan: стратегія знижки для постійних клієнтів
-class RegularDiscountPlan extends Basket {
-  // Робимо метод applyDiscount, що приймає ціну price як параметр
-  // Повертає ціну з урахуванням знижки в 10% price * 0.9
-}
-
-//Клас VIPDiscountPlan: стратегія знижки для VIP клієнтів
-class VIPDiscountPlan extends Basket {
-  // Робимо метод applyDiscount, що приймає ціну price як параметр
-  // Повертає ціну з урахуванням знижки в 20% price * 0.8
-}
-
-// Клас NewClientDiscountPlan: стратегія знижки для нових клієнтів
-class NewClientDiscountPlan extends Basket {
-  // Робимо метод applyDiscount, що приймає ціну price як параметр
-  // Повертає ціну з урахуванням знижки в 5% price * 0.95
-}
-
-console.log("Завдання 6 ====================================");
-// Після виконання розкоментуйте код нижче
-
-// Створення нового екземпляру кошика зі стратегією знижки для нових клієнтів
-// const basket1 = new Basket(new NewClientDiscountPlan());
-
-// Додавання товарів до кошика
-// basket1.addGood({ name: "Product 1", price: 100 });
-// basket1.addGood({ name: "Product 2", price: 50 });
-
-// Розрахунок і виведення загальної вартості товарів з урахуванням знижки
-// console.log(basket1.calculateTotalPrice());
+    constructor(discountPlan) {
+      this.discountPlan = discountPlan;
+    }
+    // Робимо метод addGood, що приймає один параметр - good, який потрібно додати до масиву
+    // Додаємо новий товар в масив товарів
+    addGood(good) {
+        this.goods.push(good);
+    }
+    // Робимо метод calculateTotalPrice, що розраховує загальну вартість товарів в кошику з урахуванням знижки
+    // За допомогою метода reduce ми сумуємо вартість всіх товарів в масиві
+    calculateTotalPrice() {
+        const totalPrice = this.goods.reduce((total, good) => total + good.price, 0);
+    // Застосовуємо знижку до загальної вартості за допомогою метода applyDiscount нашого об'єкта discountPlan
+    return this.discountPlan.applyDiscount(totalPrice);
+    }
+  }
+  
+  // Клас RegularDiscountPlan: стратегія знижки для постійних клієнтів
+  class RegularDiscountPlan extends Basket {
+    // Робимо метод applyDiscount, що приймає ціну price як параметр
+    // Повертає ціну з урахуванням знижки в 10% price * 0.9
+    applyDiscount(price) {
+        return price * 0.9;
+    }
+  }
+  
+  //Клас VIPDiscountPlan: стратегія знижки для VIP клієнтів
+  class VIPDiscountPlan extends Basket {
+    // Робимо метод applyDiscount, що приймає ціну price як параметр
+    // Повертає ціну з урахуванням знижки в 20% price * 0.8
+    applyDiscount(price) {
+        return price * 0.8;
+    }
+  }
+  
+  // Клас NewClientDiscountPlan: стратегія знижки для нових клієнтів
+  class NewClientDiscountPlan extends Basket {
+    // Робимо метод applyDiscount, що приймає ціну price як параметр
+    // Повертає ціну з урахуванням знижки в 5% price * 0.95
+    applyDiscount(price) {
+        return price * 0.95;
+    }
+  }
+  
+  console.log("Завдання 6 ====================================");
+  // Після виконання розкоментуйте код нижче
+  
+  // Створення нового екземпляру кошика зі стратегією знижки для нових клієнтів
+  const basket1 = new Basket(new NewClientDiscountPlan());
+  
+  // Додавання товарів до кошика
+  basket1.addGood({ name: "Product 1", price: 100 });
+  basket1.addGood({ name: "Product 2", price: 50 });
+  
+  // Розрахунок і виведення загальної вартості товарів з урахуванням знижки
+  console.log(basket1.calculateTotalPrice());
+  
